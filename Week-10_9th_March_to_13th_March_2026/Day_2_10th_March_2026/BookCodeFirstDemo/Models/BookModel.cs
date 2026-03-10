009e3b9391b993ba9607c413c702aa9d9cfe0ac0
SHIVANSHAGL05 <shivanshag05@gmail.com>
2026-03-10 15:09:12 +0530

    Added Week-10 project

---
+++ Week-10_9th_March_to_13th_March_2026/Day_2_10th_March_2026/BookCodeFirstDemo/Models/BookModel.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookCodeFirstDemo.Models
+{
+    [Table("tblBooks")]
+    public class BookModel
+    {
+        [Key]
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ Week-10_9th_March_to_13th_March_2026/Day_2_10th_March_2026/BookCodeFirstDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using BookCodeFirstDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BookDBContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("cs1")
+    ));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Book}/{action=Index}/{id?}");
+
+app.Run();
